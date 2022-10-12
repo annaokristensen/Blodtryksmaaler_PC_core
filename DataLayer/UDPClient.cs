461c28fc2bad1e531a71f4f23d1c82f715de4ff6
julierauhe <202106752@post.au.dk>
2022-10-12 08:43:59 +0200

    Startet på implementering af UDPClient-klassen

---
+++ DataLayer/UDPClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer_PC
+{
+	/// <summary>
+	/// Klassen står for kommunikationen med RPi-SW via. UDP. PC'en er Clienten
+	/// </summary>
+	class UDPClient : IDisposable
+	{
+		private const int portNo = 11000;
+
+		private UdpClient udpClient = new UdpClient(portNo);
+
+
+	}
+}
