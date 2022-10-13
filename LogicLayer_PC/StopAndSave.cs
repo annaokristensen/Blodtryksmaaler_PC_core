318b71f3ca6793c97319c636dd77698353ca5182
julierauhe <202106752@post.au.dk>
2022-10-13 10:58:14 +0200

    Oprettet StopAndSave-klasse på logiklaget

---
+++ LogicLayer_PC/StopAndSave.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO_PC;
+
+namespace LogicLayer_PC
+{
+	/// <summary>
+	/// Indeholder en metode som bliver kaldt når vi trykker på "Stop og Gem" på GUI.
+	/// Sørger for at få data ned til datalaget så det kan gemmes i databasen. 
+	/// </summary>
+	class StopAndSave
+	{
+		public void SaveMeasurement(BPMeasurementData bpMesData)
+		{
+
+		}
+	}
+}
