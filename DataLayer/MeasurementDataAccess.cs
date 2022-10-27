2a2fb6479656b99774b645c25d855f5d3f63e41a
RikkeAndersen <au694849@uni.au.dk>
2022-10-27 10:51:39 +0200

    physionetfil indlæst i measurementDataAccess

--- DataLayer/MeasurementDataAccess.cs
+++ DataLayer/MeasurementDataAccess.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Runtime.InteropServices.ComTypes;
 using DTO_PC;
+using System.IO;
 
 namespace DataLayer_PC
 {
@@ -15,15 +16,59 @@ namespace DataLayer_PC
 	class MeasurementDataAccess
 	{
 		//SqlConnection conn;
+		public List<MeasurementDataAccess>samplesList = new List<MeasurementDataAccess>();
+		public string Path = "samplestest.txt";
+		public double second { get; set; }
+		public double sampleValue { get; set; }
 
-		public MeasurementDataAccess()
+		public MeasurementDataAccess(double second, double sampleValue)
 		{
 			//conn = new SqlConnection(@"INDSÆT CONNECTION STRING TIL MEASUREMENT-DATABASE")
+			this.second = second;
+			this.sampleValue = sampleValue;
 		}
 
 		public void StoreData(BPMeasurementData_DTO bpMesData)
 		{
 			//Indsæt kode til at gemme i databasen her
 		}
-	}
+		//public void GetDataFromPhysionet()
+		//{
+		//	string inputRecord;
+		//	string[] inputFields;
+
+		//	///
+		//	//array = File.ReadAllLines()
+		//	///
+			
+		//	//Opretter filestream og format objekt 
+		//	FileStream input = new FileStream("samplestest.txt", FileMode.Open, FileAccess.Read);
+		//	StreamReader fileReader = new StreamReader(input);
+
+		//	//Så længe der er data i filen lægges det ind i listen sampleList
+		//	while((inputRecord = fileReader.ReadLine()) != null)
+		//	{
+		//		//Splitter dataen i tekstfilen op i kolonner 
+		//		inputFields = inputRecord.
+		//	}
+		
+		//}
+		public List<MeasurementDataAccess> ReadSamples()
+		{
+			List<MeasurementDataAccess> samplesList = new List<MeasurementDataAccess>();
+			List<string> holder = File.ReadAllLines(Path).ToList();
+			foreach (string sample in holder)
+			{
+				string[] input = sample.Split(' ');
+				MeasurementDataAccess s = new MeasurementDataAccess(Convert.ToInt32(input[0]), Convert.ToInt32(input[1]));
+				samplesList.Add(s);
+                
+            }
+			return samplesList;
+			Console.WriteLine(samplesList);
+
+
+
+        }
+    }
 }
--- LogicLayer_PC/MeasurementControlPC.cs
+++ LogicLayer_PC/MeasurementControlPC.cs
@@ -8,5 +8,6 @@ namespace LogicLayer_PC
     public class MeasurementControlPC
     {
 
+
     }
 }
