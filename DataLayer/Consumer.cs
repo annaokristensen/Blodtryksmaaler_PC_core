3138ed07acc8eb4c235f7745c8e9f5661264d566
RikkeAndersen <au694849@uni.au.dk>
2022-12-08 15:07:11 +0100

    Consumer class oprettet

---
+++ DataLayer/Consumer.cs
@@ -0,0 +1,37 @@
+﻿using DTO_PC;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer_PC
+{
+    public class Consumer
+    {
+        private readonly BlockingCollection<double> RawDataBlocking;
+        public BPMesDataGUI_DTO rawDataDTOBC;
+
+        public Consumer()
+        {
+            RawDataBlocking = new BlockingCollection<double>();
+        }
+        public BPMesDataGUI_DTO TakeFromBC()
+        {
+            while (!RawDataBlocking.IsCompleted) //Tjekker om der er completet i ReadSample
+            {
+                try
+                {
+                    rawDataDTOBC.RawDataList.Add(RawDataBlocking.Take());
+                    return rawDataDTOBC;
+                }
+                catch (InvalidOperationException)
+                {
+                    return null;
+                }
+            }
+            return null;
+        }
+    }
+}
